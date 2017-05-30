a09897ac25780df30d815d32ff9fd0be8bf04fad
Alexander Nelzin <asnelzin@gmail.com>
2017-05-30 22:11:20 +0300

    :shit:

---
+++ kek/k.go
@@ -0,0 +1,23 @@
+// This package contains some barbarian magic
+
+package kek
+
+import "fmt"
+
+const хуй = `Вилкой в глаз или в жопу раз?`
+
+// Public
+func Привет() {
+	fmt.Println(`¯\_(ツ)_/¯`)
+}
+
+func НаЗаборе() {
+	fmt.Println(хуй)
+}
+
+// Private
+func пока() {}
+
+func 独角兽() string {
+	return "🦄"
+}
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/asnelzin/k/kek"
+)
+
+func main() {
+	kek.Привет()
+	kek.НаЗаборе()
+}
